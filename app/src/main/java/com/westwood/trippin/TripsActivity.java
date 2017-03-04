443387bd61d7305248d148ee360c6704c6c00585
Westwood <annechen112995@gmail.com>
2017-03-04 03:07:56 -0800

    pagersliding tabs

--- app/src/main/java/com/westwood/trippin/TripsActivity.java
+++ app/src/main/java/com/westwood/trippin/TripsActivity.java
@@ -1,14 +1,19 @@
 package com.westwood.trippin;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
+
+import com.astuetz.PagerSlidingTabStrip;
 
 public class TripsActivity extends AppCompatActivity {
 
     SmartFragmentStatePagerAdapter adapterViewPager;
     ViewPager vpPager;
+    PagerSlidingTabStrip tabsStrip;
 
     public TripsActivity(SmartFragmentStatePagerAdapter adapterViewPager) {
         this.adapterViewPager = adapterViewPager;
@@ -23,10 +28,46 @@ public class TripsActivity extends AppCompatActivity {
 
         vpPager = (ViewPager) findViewById(R.id.vpPager);
 
-        //Set the viewpager adapter for the pager
-        //adapterViewPager = new TripsPagerAdapter();
-        //vpPager.setAdapter(adapterViewPager);
+        vpPager.setAdapter(new SmartFragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return null;
+            }
 
+            @Override
+            public int getCount() {
+                return 0;
+            }
+        });
+
+        // Give the PagerSlidingTabStrip the ViewPager
+        tabsStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+
+        // Attach the view pager to the tab strip
+        tabsStrip.setViewPager(vpPager);
+
+        tabsStrip.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            // This method will be invoked when a new page becomes selected.
+            @Override
+            public void onPageSelected(int position) {
+                Toast.makeText(TripsActivity.this,
+                        "Selected page position: " + position, Toast.LENGTH_SHORT).show();
+            }
+
+            // This method will be invoked when the current page is scrolled
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                // Code goes here
+            }
+
+            // Called when the scroll state changes:
+            // SCROLL_STATE_IDLE, SCROLL_STATE_DRAGGING, SCROLL_STATE_SETTLING
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                // Code goes here
+            }
+        });
     }
 
 }
