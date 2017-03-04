baa7e130b1eb4afeb960a78b3beb1d49978f2775
Westwood <annechen112995@gmail.com>
2017-03-04 04:15:50 -0800

    temp

--- app/src/main/java/com/westwood/trippin/TripsActivity.java
+++ app/src/main/java/com/westwood/trippin/TripsActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.astuetz.PagerSlidingTabStrip;
