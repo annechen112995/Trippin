1b31bbf39e8a8cbf042135b3e6fc6569c92fea8e
Lynn Ngo <thaoly.ngo@gmail.com>
2017-03-05 02:26:18 -0800

    eh

--- app/src/main/java/com/westwood/trippin/GalleryFragment.java
+++ app/src/main/java/com/westwood/trippin/GalleryFragment.java
@@ -1,5 +1,5 @@
 package com.westwood.trippin;
-//compile "com.android.support:recyclerview-v7:24.2."
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.widget.GridView;
@@ -7,15 +7,21 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.view.View;
 
+
+
 public class GalleryFragment extends Fragment {
+    ImageAdapter myImageAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle SavedInstanceState ) {
-        View view = inflater.inflate(R.layout.fragment_gallery, container, false);
-        GridView gridView = (GridView) view.findViewById(R.id.gridView);
-        gridView.setAdapter(new ImageAdapter(getContext()));
-        return view;
+        View rootview = inflater.inflate(R.layout.fragment_gallery, container, false);
+        GridView gridView = (GridView) rootview.findViewById(R.id.gridView);
+        myImageAdapter = new ImageAdapter(getContext());
+        gridView.setAdapter(myImageAdapter);
+
+
+        return rootview;
     }
 
 
