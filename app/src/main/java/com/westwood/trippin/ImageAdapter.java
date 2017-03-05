aa24824249e297604216146e22bc6c0b1208195c
Chih-Chieh Lee <clee7@caltech.edu>
2017-03-04 18:49:05 -0800

    minor changes

--- app/src/main/java/com/westwood/trippin/ImageAdapter.java
+++ app/src/main/java/com/westwood/trippin/ImageAdapter.java
@@ -6,7 +6,6 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.GridView;
-import android.view.
 
 public class ImageAdapter extends BaseAdapter {
     private Context context;
@@ -22,7 +21,6 @@ public class ImageAdapter extends BaseAdapter {
             R.drawable.img15,R.drawable.img16,
             R.drawable.img17,R.drawable.img18,
             R.drawable.img19,R.drawable.img20
-            R.drawable.img19,R.drawable.img
     };
 
     public ImageAdapter(Context c){
