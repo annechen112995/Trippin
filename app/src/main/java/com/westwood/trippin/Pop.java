e0037c418e5ec2a39bd37d2be25050c49acdaaee
Chih-Chieh Lee <clee7@caltech.edu>
2017-03-05 06:38:32 -0800

    slight change

--- app/src/main/java/com/westwood/trippin/Pop.java
+++ app/src/main/java/com/westwood/trippin/Pop.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,6 +30,7 @@ public class Pop extends Activity {
         int height = dm.heightPixels;
 
         getWindow().setLayout((int) (0.9 * width), (int) (0.9 * height));
+        getWindow().setGravity(Gravity.CENTER);
 
         cost = (EditText) findViewById(R.id.expCost);
         name = (EditText) findViewById(R.id.expName);
