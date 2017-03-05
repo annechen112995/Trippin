37833da45e6b32c9a1f22af7f088d8cd622675ed
Westwood <annechen112995@gmail.com>
2017-03-04 20:02:55 -0800

    Packing list update

--- app/src/main/java/com/westwood/trippin/PackingFragment.java
+++ app/src/main/java/com/westwood/trippin/PackingFragment.java
@@ -7,12 +7,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class PackingFragment extends Fragment {
     public static final String ARG_PAGE = "ARG_PAGE";
-
-    private int mPage;
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
+    private Button btnAddItems;
+    private EditText etNewItem;
 
     public static PackingFragment newInstance(int page) {
         Bundle args = new Bundle();
@@ -33,8 +43,47 @@ public class PackingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_packing, container, false);
-        TextView tvTitle = (TextView) view.findViewById(R.id.tvTitle);
-        //tvTitle.setText("Fragment #" + mPage);
+
+        lvItems = (ListView) view.findViewById(R.id.lvItems);
+        btnAddItems = (Button) view.findViewById(R.id.btnAddItem);
+        etNewItem = (EditText) view.findViewById(R.id.etNewItem);
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, items);
+        items.add("Umbrella.....jk");
+        items.add("Hot Cheetos ftw");
+        items.add("Polaroid Cam");
+
+        setupListViewListener();
+
+        btnAddItems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String itemText = etNewItem.getText().toString();
+                itemsAdapter.add(itemText);
+                etNewItem.setText("");
+            }
+        });
+
+        lvItems.setAdapter(itemsAdapter);
+
         return view;
     }
+
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        // Remove the item within array at position
+                        items.remove(pos);
+                        // Refresh the adapter
+                        itemsAdapter.notifyDataSetChanged();
+                        // Return true consumes the long click event (marks it handled)
+                        return true;
+                    }
+
+                });
+    }
+
 }
